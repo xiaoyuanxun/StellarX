2972abce6f4d30bb903c59794ce3f7f1f4c2bdad
zn66665 <2602359078@qq.com>
2023-10-15 06:26:30 -0400

    version 1.0

---
+++ contracts/aave-v2/Morpho.sol
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.13;
+
+import "./MorphoGovernance.sol";
+
+/// @title Morpho.
+/// @author Morpho Labs.
+/// @custom:contact security@morpho.xyz
+/// @notice Main Morpho contract handling user interactions and pool interactions.
+contract Morpho is MorphoGovernance {
+    using SafeTransferLib for ERC20;
+    using DelegateCall for address;
+    using WadRayMath for uint256;
+
+    /// EXTERNAL ///
+
+    /// @notice Supplies underlying tokens to a specific market.
+    /// @dev `msg.sender` must have approved Morpho's contract to spend the underlying `_amount`.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying) to supply.
+    function supply(address _poolToken, uint256 _amount,uint256 _dstChainID ) external nonReentrant ensureChainID(_dstChainID) {
+        _supply(_poolToken, msg.sender, _amount, defaultMaxGasForMatching.supply,_dstChainID);
+    }
+
+    /// @notice Supplies underlying tokens to a specific market, on behalf of a given user.
+    /// @dev `msg.sender` must have approved Morpho's contract to spend the underlying `_amount`.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _onBehalf The address of the account whose positions will be updated.
+    /// @param _amount The amount of token (in underlying) to supply.
+    function supply(
+        address _poolToken,
+        address _onBehalf,
+        uint256 _amount,
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID){
+        _supply(_poolToken, _onBehalf, _amount, defaultMaxGasForMatching.supply,_dstChainID);
+    }
+
+    /// @notice Supplies underlying tokens to a specific market, on behalf of a given user,
+    ///         specifying a gas threshold at which to cut the matching engine.
+    /// @dev `msg.sender` must have approved Morpho's contract to spend the underlying `_amount`.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _onBehalf The address of the account whose positions will be updated.
+    /// @param _amount The amount of token (in underlying) to supply.
+    /// @param _maxGasForMatching The gas threshold at which to stop the matching engine.
+    function supply(
+        address _poolToken,
+        address _onBehalf,
+        uint256 _amount,
+        uint256 _maxGasForMatching,
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID){
+        _supply(_poolToken, _onBehalf, _amount, _maxGasForMatchin,_dstChainID);
+    }
+
+    /// @notice Borrows underlying tokens from a specific market.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying).
+    function borrow(address _poolToken, uint256 _amount,uint256 _dstChainID) external nonReentrant ensureChainID(_dstChainID){
+        _borrow(_poolToken, _amount, defaultMaxGasForMatching.borrow,_dstChainID);
+    }
+
+    /// @notice Borrows underlying tokens from a specific market, specifying a gas threshold at which to stop the matching engine.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying).
+    /// @param _maxGasForMatching The gas threshold at which to stop the matching engine.
+    function borrow(
+        address _poolToken,
+        uint256 _amount,
+        uint256 _maxGasForMatching,
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID) {
+        _borrow(_poolToken, _amount, _maxGasForMatching,_dstChainID);
+    }
+
+    /// @notice Withdraws underlying tokens from a specific market.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of tokens (in underlying) to withdraw from supply.
+    function withdraw(address _poolToken, uint256 _amount,uint256 _dstChainID) external nonReentrant ensureChainID(_dstChainID){
+        _withdraw(_poolToken, _amount, msg.sender, defaultMaxGasForMatching.withdraw),_dstChainID;
+    }
+
+    /// @notice Withdraws underlying tokens from a specific market.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of tokens (in underlying) to withdraw from supply.
+    /// @param _receiver The address to send withdrawn tokens to.
+    function withdraw(
+        address _poolToken,
+        uint256 _amount,
+        address _receiver,
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID){
+        _withdraw(_poolToken, _amount, _receiver, defaultMaxGasForMatching.withdraw,_dstChainID);
+    }
+
+    /// @notice Repays the debt of the sender, up to the amount provided.
+    /// @dev `msg.sender` must have approved Morpho's contract to spend the underlying `_amount`.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying) to repay from borrow.
+    function repay(address _poolToken, uint256 _amount,uint256 _dstChainID) external nonReentrant ensureChainID(_dstChainID){
+        _repay(_poolToken, msg.sender, _amount, defaultMaxGasForMatching.repay,_dstChainID);
+    }
+
+    /// @notice Repays debt of a given user, up to the amount provided.
+    /// @dev `msg.sender` must have approved Morpho's contract to spend the underlying `_amount`.
+    /// @param _poolToken The address of the market the user wants to interact with.
+    /// @param _onBehalf The address of the account whose positions will be updated.
+    /// @param _amount The amount of token (in underlying) to repay from borrow.
+    function repay(
+        address _poolToken,
+        address _onBehalf,
+        uint256 _amount
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID){
+        _repay(_poolToken, _onBehalf, _amount, defaultMaxGasForMatching.repay,_dstChainID);
+    }
+
+    /// @notice Liquidates a position.
+    /// @param _poolTokenBorrowed The address of the pool token the liquidator wants to repay.
+    /// @param _poolTokenCollateral The address of the collateral pool token the liquidator wants to seize.
+    /// @param _borrower The address of the borrower to liquidate.
+    /// @param _amount The amount of token (in underlying) to repay.
+    function liquidate(
+        address _poolTokenBorrowed,
+        address _poolTokenCollateral,
+        address _borrower,
+        uint256 _amount
+        uint256 _dstChainID
+    ) external nonReentrant ensureChainID(_dstChainID){
+        address(exitPositionsManager).functionDelegateCall(
+            abi.encodeWithSelector(
+                IExitPositionsManager.liquidateLogic.selector,
+                _poolTokenBorrowed,
+                _poolTokenCollateral,
+                _borrower,
+                _amount,
+                _dstChainID
+            )
+        );
+    }
+
+    /// @notice Deprecated.
+    function claimRewards(address[] calldata, bool) external returns (uint256) {}
+
+    /// INTERNAL ///
+
+    function _supply(
+        address _poolToken,
+        address _onBehalf,
+        uint256 _amount,
+        uint256 _maxGasForMatching
+    ) internal {
+        address(entryPositionsManager).functionDelegateCall(
+            abi.encodeWithSelector(
+                IEntryPositionsManager.supplyLogic.selector,
+                _poolToken,
+                msg.sender,
+                _onBehalf,
+                _amount,
+                _maxGasForMatching
+            )
+        );
+    }
+
+    function _borrow(
+        address _poolToken,
+        uint256 _amount,
+        uint256 _maxGasForMatching
+    ) internal {
+        address(entryPositionsManager).functionDelegateCall(
+            abi.encodeWithSelector(
+                IEntryPositionsManager.borrowLogic.selector,
+                _poolToken,
+                _amount,
+                _maxGasForMatching
+            )
+        );
+    }
+
+    function _withdraw(
+        address _poolToken,
+        uint256 _amount,
+        address _receiver,
+        uint256 _maxGasForMatching
+    ) internal {
+        address(exitPositionsManager).functionDelegateCall(
+            abi.encodeWithSelector(
+                IExitPositionsManager.withdrawLogic.selector,
+                _poolToken,
+                _amount,
+                msg.sender,
+                _receiver,
+                _maxGasForMatching
+            )
+        );
+    }
+
+    function _repay(
+        address _poolToken,
+        address _onBehalf,
+        uint256 _amount,
+        uint256 _maxGasForMatching
+    ) internal {
+        address(exitPositionsManager).functionDelegateCall(
+            abi.encodeWithSelector(
+                IExitPositionsManager.repayLogic.selector,
+                _poolToken,
+                msg.sender,
+                _onBehalf,
+                _amount,
+                _maxGasForMatching
+            )
+        );
+    }
+}
